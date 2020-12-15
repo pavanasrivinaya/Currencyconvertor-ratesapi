8eefbceb858bf904c49511f0718d56a9cb61dfe0
Pavana sri vinaya <pavanasri.nyros@gmail.com>
2020-12-15 13:58:43 +0530

    first

---
+++ dist/precache-manifest.75ca472daca983d69a7d2535158dddb8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bf022439b1ea32f03a5",
+    "url": "/css/app.5d042fe4.css"
+  },
+  {
+    "revision": "3424f265e439ed4619f0",
+    "url": "/css/chunk-vendors.8b220ae0.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "8f7baee2bd5a44e2265d194fa609d7b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "399b023a509f054e16a9",
+    "url": "/js/about.a7132713.js"
+  },
+  {
+    "revision": "5bf022439b1ea32f03a5",
+    "url": "/js/app.63222ff2.js"
+  },
+  {
+    "revision": "3424f265e439ed4619f0",
+    "url": "/js/chunk-vendors.b3509591.js"
+  },
+  {
+    "revision": "74e6a489c35a8e384bafb3ff72bb0501",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
